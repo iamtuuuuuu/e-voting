db9ffd1de7f4d01a43b97abf9d2ecf894b6996d9
SotaTek-TuVu <tu.vu@sotatek.com>
2022-06-08 16:09:32 +0700

    fn main

--- programs/first-app/src/lib.rs
+++ programs/first-app/src/lib.rs
@@ -6,17 +6,25 @@ pub mod schema;
 pub use schema::*;
 
 pub mod instructions;
-pub use instruction::*;
+pub use instructions::*;
 
 pub mod errors;
-pub use error::*;
+pub use errors::*;
 
 #[program]
 pub mod first_app {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, start_date: i64, end_date: i64) -> Result<()> {
+        initialize_candidate::exec(ctx, start_date, end_date)
+    }
+
+    pub fn vote(ctx: Context<Vote>, amount: u64) -> Result<()> {
+        vote::exec(ctx, amount)
+    }
+
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        close::exec(ctx)
     }
 }
 
